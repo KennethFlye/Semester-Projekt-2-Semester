8c70c95602d8d1dbe0e74e33553391c073b58b54
oskarbucn <10434661@ucn.dk>
2022-11-30 14:58:42 +0100

    Gokart done

--- src/database/GokartDB.java
+++ src/database/GokartDB.java
@@ -11,7 +11,7 @@ public class GokartDB implements GokartDBIF {
 	
 	private PreparedStatement getAllGokarts;
 	public PreparedStatement getInfoForRentedGokart;
-	public static PreparedStatement getTimeForRentOut;
+	public PreparedStatement getTimeForRentOut;
 	
 	public GokartDB() throws SQLException {
 		getAllGokarts = DBConnection.getInstance().getConnection().prepareStatement(getAllGokartsQ);
@@ -20,6 +20,15 @@ public class GokartDB implements GokartDBIF {
 	}
 
 	
+	/**
+	 * Checks whether the required amount of gokarts are available for a booking. A gokart is available if it is not
+	 * rented out at the time of the booking. The method works by finding each gokart's rentouts, and checking whether 
+	 * any rentouts overlap the booking.
+	 * @return true if enough gokarts are available, false otherwise
+	 * @param amount gokarts required
+	 * @param start start date and time for booking 
+	 * @param end date and time for booking
+	 */
 	@Override
 	public boolean checkGokarts(int amount, LocalDateTime start, LocalDateTime end) throws SQLException {
 		int available = 0;
@@ -40,18 +49,35 @@ public class GokartDB implements GokartDBIF {
 				getTimeForRentOut.setInt(1, rentId);
 				ResultSet rsTimes = getTimeForRentOut.executeQuery();
 				
-				//String rentOutStart = rsTimes.getString
+				rsTimes.next();
+				String rentOutStart = rsTimes.getString("startTime");
+				String rentOutEnd = rsTimes.getString("endTime");
+				String checkStart = start.toString();
+				String checkEnd = end.toString();
 				
+				if (rentOutStart.compareTo(checkStart) >= 0 && rentOutStart.compareTo(checkEnd) <= 0) {
+					rented = true;
+				}
+				else if(rentOutEnd.compareTo(checkStart) >= 0 &&  rentOutEnd.compareTo(checkEnd) <= 0) {
+					rented = true;
+				}
+				else if(rentOutStart.compareTo(checkStart) <= 0 && rentOutEnd.compareTo(checkEnd) >= 0) {
+					rented = true;
+				}
 				
+			}
+			if (!rented) {
+				available++;
 			}
 			
 			
 		}
+		return (available >= amount);
 		
-		return true;
 	}
 	
-	public static void main(String[] args) {
+	/**
+	 * public static void main(String[] args) {
 		try{ 
 		getTimeForRentOut = DBConnection.getInstance().getConnection().prepareStatement(getTimeForRentOutQ);
 		getTimeForRentOut.setInt(1, 1);
@@ -65,5 +91,7 @@ public class GokartDB implements GokartDBIF {
 			
 		}
 	}
+	 */
+	
 
 }
