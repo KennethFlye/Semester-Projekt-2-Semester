d9caa8870db36c17bfcbb60cc887653eb9eec097
MagnusPC <10434658@ucn.dk>
2022-11-30 10:47:27 +0100

    Update CustomerCtrl.java

--- src/controller/CustomerCtrl.java
+++ src/controller/CustomerCtrl.java
@@ -7,12 +7,12 @@ import model.Customer;
 public class CustomerCtrl {
 	private CustomerDBIF customerDBIF;
 
-	public CustomerCtrl() /*throws SQLException**/ {
+	public CustomerCtrl() /*throws DataAccessException*/ {
 		//TODO remove cast to IF - let db implement IF instead
 		customerDBIF = (CustomerDBIF) new CustomerDB();
 	}
 	
-	public Customer findCustomer(String phoneNo) /*throws SQLException*/ {
+	public Customer findCustomer(String phoneNo) /*throws DataAccessException*/ {
 		return customerDBIF.findCustomer(phoneNo);
 	}
 
