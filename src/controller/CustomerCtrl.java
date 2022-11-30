0f667457c3a3d7f349d30c666d97d2116edef12c
MagnusPC <10434658@ucn.dk>
2022-11-30 11:41:02 +0100

    Update CustomerCtrl.java

--- src/controller/CustomerCtrl.java
+++ src/controller/CustomerCtrl.java
@@ -1,18 +1,18 @@
 package controller;
 
-import database.CustomerDBIF;
 import database.CustomerDB;
+import database.CustomerDBIF;
+import database.DataAccessException;
 import model.Customer;
 
 public class CustomerCtrl {
 	private CustomerDBIF customerDBIF;
 
-	public CustomerCtrl() /*throws DataAccessException*/ {
-		//TODO remove cast to IF - let db implement IF instead
-		customerDBIF = (CustomerDBIF) new CustomerDB();
+	public CustomerCtrl() throws DataAccessException {
+		customerDBIF = new CustomerDB();
 	}
 	
-	public Customer findCustomer(String phoneNo) /*throws DataAccessException*/ {
+	public Customer findCustomer(String phoneNo) throws DataAccessException {
 		return customerDBIF.findCustomer(phoneNo);
 	}
 
