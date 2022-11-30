e36b681c5b6872ba765b190dc5f673bfd4913b3e
KennethFlye <10434642@ucn.dk>
2022-11-30 11:34:25 +0100

    CustomerDB Opsat
    udvidelse af Contact og Customer

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,10 +1,35 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Customer extends Contact {
 	
-	public Customer(String name, String phoneNo, String email) {
-		super(name, phoneNo, email);
+	private LocalDate dateOfBirth;
+	
+	public Customer(String name, String phoneNo) {
+		super(name, phoneNo);
 		System.out.println(this + " created");
 	}
+	
+	public Customer(String name, String phoneNo, String email, String address, int zipCode, String city, String country, LocalDate dateOfBirth) {
+		super(name, phoneNo, email, address, zipCode, city, country);
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	/**
+	 * @return the dateOfBirth
+	 */
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	/**
+	 * @param dateOfBirth the dateOfBirth to set
+	 */
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	
+	
 
 }
