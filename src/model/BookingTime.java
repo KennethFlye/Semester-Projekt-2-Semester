a617203725c301899f3d6763ea012ef1ecf306ad
jeppeSiegfredsen <10438303@ucn.dk>
2022-12-01 08:54:25 +0100

    det er booking tid

--- src/model/BookingTime.java
+++ src/model/BookingTime.java
@@ -1,14 +1,44 @@
 package model;
 
+
 import java.time.LocalDateTime;
+
+import model.EventType.EnumType;
 
 public class BookingTime {
 
+	
+	EnumType eventType; 
 	LocalDateTime startTime, finishTime;
 	
-	public BookingTime(LocalDateTime startTime, LocalDateTime finishTime) {
+	
+
+	public static void main(String[] args) {
+		
+		BookingTime bt = new BookingTime("Formel 1", LocalDateTime.now(), LocalDateTime.now());
+		System.out.println(bt.eventType); }
+	
+	public BookingTime(String type, LocalDateTime startTime, LocalDateTime finishTime) {
+		this.eventType = EnumType.valueOfLabel(type);
 		this.finishTime=finishTime;
 		this.startTime=startTime;
 	}
+	public EnumType getEventType() {
+		return eventType;
+	}
+	
+	public LocalDateTime getStartTime() {
+		return startTime;
+	}
+
+	public LocalDateTime getFinishTime() {
+		return finishTime;
+	}
+	
+	
+}
+				
+
+	
 	
 }
