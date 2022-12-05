e85f205eccb2b3d6be0b30c818e77271b5fbef27
jeppeSiegfredsen <10438303@ucn.dk>
2022-12-05 12:28:57 +0100

    Update TestAddTimeSlot.java

--- src/test/TestAddTimeSlot.java
+++ src/test/TestAddTimeSlot.java
@@ -17,31 +17,49 @@ import model.EventType.EnumType;
 
 class TestAddTimeSlot {
 
-	static DBConnection con = null;
+	static DBConnection con;
 	static BookingCtrl bc;
+	static LocalDateTime d;
+	
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		con = DBConnection.getInstance();
+		bc = new BookingCtrl();
+		d = LocalDateTime.of(2022, 11, 30, 12, 0);
 	
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		con = null;
+		bc = null;
+		d = null;
+	
 	}
 		
+	
+	
 	@Test
-	void test() throws DataAccessException {
+	void testT() throws DataAccessException {
 		//Arrange
 		bc.createBooking();
 		EventType et = new EventType(EnumType.FORMULA_1);
+<<<<<<< Updated upstream
 		LocalDateTime startTime = LocalDateTime.of(2022, 11, 30, 12, 0);
+=======
+		BookingTime bt = new BookingTime(et, d);
+>>>>>>> Stashed changes
 		
 		
 		//Act
 		bc.addTimeslot(et.getEnumType().getLabel(), startTime);
 		//Assert
+<<<<<<< Updated upstream
 		//assertEquals();
+=======
+		assertEquals( d.plusMinutes(et.getEnumType().getLength()),bt.getFinishTime());
+>>>>>>> Stashed changes
 	}
 
 }
