d3e57dc5c575f6d92e806e2786866976c5a1278c
oskarbucn <10434661@ucn.dk>
2022-12-14 10:03:48 +0100

    Update BookingTimeDB.java

--- src/database/BookingTimeDB.java
+++ src/database/BookingTimeDB.java
@@ -102,7 +102,7 @@ public class BookingTimeDB implements BookingTimeDBIF {
 		
 		private BookingTime buildObject(ResultSet rs) throws SQLException {
 			EventType tempEvent = new EventType(EnumType.valueOfLabel(rs.getString("eventType")));
-			BookingTime bt = new BookingTime(tempEvent, rs.getTimestamp("startTime").toLocalDateTime(), rs.getTimestamp("finishTime").toLocalDateTime());
+			BookingTime bt = new BookingTime(tempEvent, rs.getTimestamp("startTime").toLocalDateTime(), 1);
 			return bt;
 		}
 	
@@ -131,7 +131,6 @@ public class BookingTimeDB implements BookingTimeDBIF {
 			for (int i = 0; i < btList.size(); i++) {
 				boolean inFront = false, inFront2 = false;
 				ZoneOffset zo = ZoneOffset.systemDefault().getRules().getOffset(Instant.now());
-					if(!checkForSameBooking(btList.get(i), bookingId)) {
 						
 						long timeA = btList.get(i).getStartTime().toEpochSecond(zo);
 						long timeB = btList.get(i).getFinishTime().toEpochSecond(zo);
@@ -151,7 +150,7 @@ public class BookingTimeDB implements BookingTimeDBIF {
 							returnBool = false;
 				}
 				
-					}
+					
 						
 				
 				
